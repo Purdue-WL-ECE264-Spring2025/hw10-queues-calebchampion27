d1fcf67115eed63efb16cb1e3ada09116caff2d9
Caleb Champion <cchampi@eceprog4.ecn.purdue.edu>
2025-04-04 15:52:01 -0400

    linked list

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,93 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) {
+  struct list_node *node = malloc(sizeof(struct list_node));
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  if (node == NULL) {  //malloc fails
+    return NULL;
+  }
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+  //initialize with value & point to null
+  node->value = value;
+  node->next = NULL;
+  
+  return node;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);  //make a new node point to NULL
+  if (node == NULL) {return 0;}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  node->next = list->head;  //point new node to the current beginning
+  list->head = node;  //change new head to new node
 
-void free_list(struct linked_list list) {}
+}
+
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);  //make a new node point to NULL
+  if (node == NULL) {return;}
+
+  if (list->head == NULL) {  //if list is empty
+    list->head = node;
+  }
+
+  //find end of linked list, then insert "node" at the end
+  struct list_node *cur = list->head;  //make new node cur to track
+  while (cur->next != NULL) {  //while the current node++ isnt equal to the end
+    cur = cur->next;
+  }
+  cur = cur->next;
+
+}
+
+size_t remove_from_head(struct linked_list *list) {
+  if (list->head == NULL) {return 0;}  //there is nothing to delete
+
+  struct list_node *temp = list->head;  //temp as new head
+  size_t value = temp->value;
+
+  list->head = temp->next;  //new head
+
+  free(temp);  //free the past node
+
+  return 0;
+}
+
+size_t remove_from_tail(struct linked_list *list) {
+  if (list->head == NULL) {return 0;}  //nothing to delete
+
+  struct list_node *temp = list->head;  //temporary
+  size_t value = temp->value;
+  size_t next = temp->next;
+
+  if (next == NULL) {  //only 1 node in lists
+    free(temp);
+    list->head = NULL;
+    return 0;
+  }
+
+  while (temp->next->next != NULL) {  //move on 1 by 1 till the end
+    temp = temp->next;
+  }
+
+  size_t value = temp->next->value;
+  free(temp->next);
+  temp->next = NULL;
+  
+  return 0;
+}
+
+void free_list(struct linked_list list) {
+  struct list_node *cur = list.head;
+  
+  while (cur != NULL) {  //free each node in list
+    struct list_node *temp = cur;
+    cur = cur->next;
+    free(temp);
+  }
+
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
