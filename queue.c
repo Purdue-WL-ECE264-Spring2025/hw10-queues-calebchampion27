cfdd9cdbde8dbbae0737605db7cc7d1b72eb5e82
Caleb Champion <cchampi@eceprog4.ecn.purdue.edu>
2025-04-04 16:00:38 -0400

    done

--- queue.c
+++ queue.c
@@ -1,8 +1,18 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+  insert_at_tail(&q->data, serialize(state));
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+  uint64_t encoded = remove_from_head(&q->data);
+  return deserialize(encoded);
+  
+  //return (struct game_state){0};
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) {
+  return start.num_steps;
+  
+}
